c345c2c13a209b54abbff12c59d49be17f0b3378
fabio-souza <101989744+fabio-leitesouza@users.noreply.github.com>
2024-09-27 16:31:50 -0300

    Update PlayerBehaviour.cs

--- Assets/Scripts/Player/PlayerBehaviour.cs
+++ Assets/Scripts/Player/PlayerBehaviour.cs
@@ -1,16 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
+    private InputManager inputManager;
     void Start()
     {
-        Debug.Log("Hello World!");
+        inputManager = new InputManager();        
+    }
+    private void Update()
+    {
+        float moveDirection - inputManager.Momevent;
+        transform.Translate(moveDirection, 0, 0);
     }
 
-    void Update()
-    {
-        
-    }
 }
