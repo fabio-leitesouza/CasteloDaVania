3e9af361e770d1bc85ebf327092356f6af3cfa55
fabio-souza <101989744+fabio-leitesouza@users.noreply.github.com>
2024-10-07 17:25:43 -0300

    update

--- Assets/Scripts/Player/PlayerAnim.cs
+++ Assets/Scripts/Player/PlayerAnim.cs
@@ -3,14 +3,17 @@ using UnityEngine;
 public class PlayerAnim : MonoBehaviour
 {
     private Animator animator;
+    private IsGroundChecker groundedChecker;
    
     private void Awake()
     {
         animator = GetComponent<Animator> ();
+        groundedChecker = GetComponent<IsGroundChecker>();
     }
     private void Update()
     {
         bool isWalking = GameManager.Instance.inputManager.Movement != 0;
         animator.SetBool("isWalking", isWalking);
+        animator.SetBool("isJumping", !groundedChecker.IsGrounded());
     }
 }
