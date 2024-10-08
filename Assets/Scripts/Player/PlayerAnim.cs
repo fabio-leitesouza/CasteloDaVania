a5f74104620eb4829b6c4e5131e1a05c3d6696c1
fabio-souza <101989744+fabio-leitesouza@users.noreply.github.com>
2024-10-08 13:57:46 -0300

    update

--- Assets/Scripts/Player/PlayerAnim.cs
+++ Assets/Scripts/Player/PlayerAnim.cs
@@ -4,16 +4,24 @@ public class PlayerAnim : MonoBehaviour
 {
     private Animator animator;
     private IsGroundChecker groundedChecker;
+    private Health playerHealth;
    
     private void Awake()
     {
         animator = GetComponent<Animator> ();
         groundedChecker = GetComponent<IsGroundChecker>();
+        playerHealth = GetComponent<Health>();
+
+        playerHealth.OnHurt += PlayHurtAnim;
     }
     private void Update()
     {
         bool isWalking = GameManager.Instance.inputManager.Movement != 0;
         animator.SetBool("isWalking", isWalking);
         animator.SetBool("isJumping", !groundedChecker.IsGrounded());
+    }
+    private void PlayHurtAnim()
+    {
+        animator.SetTrigger("hurt");
     }
 }
