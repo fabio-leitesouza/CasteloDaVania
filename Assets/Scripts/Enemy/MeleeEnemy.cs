78f80eefbdab9070fb4f72059bc6e042c55319a1
fabio-souza <101989744+fabio-leitesouza@users.noreply.github.com>
2024-10-09 13:12:42 -0300

    update

--- Assets/Scripts/Enemy/MeleeEnemy.cs
+++ Assets/Scripts/Enemy/MeleeEnemy.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 
-
 public class MeleeEnemy : BaseEnemy
 {
     [Header("Attack properties")]
@@ -14,14 +13,22 @@ public class MeleeEnemy : BaseEnemy
 
     private float cooldownTimer;
 
+    protected override void Awake()
+    {
+        base.Awake();
+        base.health.OnHurt += PlayHurtAudio;
+        base.health.OnDead += PlayDeadAudio;
+    }
+
     protected override void Update()
     {
         cooldownTimer += Time.deltaTime;
         VerifyCanAttack();
     }
+
     private void VerifyCanAttack()
     {
-        if (cooldownTimer < attackCooldown) return;
+        if (cooldownTimer < attackCooldown || canAttack == false) return;
         if (PlayerInSight())
         {
             animator.SetTrigger("attack");
@@ -31,7 +38,7 @@ public class MeleeEnemy : BaseEnemy
 
     private void AttackPlayer()
     {
-        //audioSource.clip = audioClips[0];
+        audioSource.clip = audioClips[0];
         cooldownTimer = 0;
         if (CheckPlayerInDetectArea().TryGetComponent(out Health playerHealth))
         {
@@ -49,6 +56,18 @@ public class MeleeEnemy : BaseEnemy
     {
         Collider2D playerCollider = CheckPlayerInDetectArea();
         return playerCollider != null;
+    }
+
+    private void PlayHurtAudio()
+    {
+        audioSource.clip = audioClips[1];
+        audioSource.Play();
+    }
+
+    private void PlayDeadAudio()
+    {
+        //audioSource.clip = audioClips[2];
+        audioSource.Play();
     }
 
     private void OnDrawGizmos()
